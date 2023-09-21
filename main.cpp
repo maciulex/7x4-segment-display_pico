8ce1946274b0b1ff9fc015bcd719c9ac26d8de4e
maciulex <41756057+maciulex@users.noreply.github.com>
2023-09-21 16:49:25 +0200

    1.0

---
+++ main.cpp
@@ -0,0 +1,31 @@
+//#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+
+#include "segment_display.cpp"
+
+uint16_t counter = 0;
+
+int main() {
+    stdio_init_all();
+    SEGMENT_DISPLAY::init();
+
+    multicore_launch_core1(SEGMENT_DISPLAY::display_loop);
+    while(1){
+        SEGMENT_DISPLAY::put_digits(counter);
+        counter++;
+        if (!SEGMENT_DISPLAY::DOTS_STATUS[0]) {
+            SEGMENT_DISPLAY::DOTS_STATUS[0] = 1;
+            SEGMENT_DISPLAY::DOTS_STATUS[1] = 1;
+            SEGMENT_DISPLAY::DOTS_STATUS[2] = 1;
+            SEGMENT_DISPLAY::DOTS_STATUS[3] = 1;
+        } else {
+            SEGMENT_DISPLAY::DOTS_STATUS[0] = 0;
+            SEGMENT_DISPLAY::DOTS_STATUS[1] = 0;
+            SEGMENT_DISPLAY::DOTS_STATUS[2] = 0;
+            SEGMENT_DISPLAY::DOTS_STATUS[3] = 0;
+        }
+        sleep_ms(1000);
+    }
+    return 0;
+}
---
+++ segment_display.cpp
@@ -0,0 +1,159 @@
+#ifndef SEGMENT_DISPLAY_F
+#define SEGMENT_DISPLAY_F
+#include <stdio.h>
+
+//printf(BYTE_TO_BINARY_PATTERN,  BYTE_TO_BINARY(<<>>));
+#ifndef INT_TO_BIN
+#define INT_TO_BIN
+    #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+    #define BYTE_TO_BINARY(byte)  \
+    (byte & 0x80 ? '1' : '0'), \
+    (byte & 0x40 ? '1' : '0'), \
+    (byte & 0x20 ? '1' : '0'), \
+    (byte & 0x10 ? '1' : '0'), \
+    (byte & 0x08 ? '1' : '0'), \
+    (byte & 0x04 ? '1' : '0'), \
+    (byte & 0x02 ? '1' : '0'), \
+    (byte & 0x01 ? '1' : '0') 
+#endif
+
+namespace SEGMENT_DISPLAY {
+    #define NUMBER_OF_SECTORS 4
+
+    //A,B,C,D,E,F,G,DP
+    uint8_t DATA_PINS[] {
+        1,5,9,7,6,2,10,8
+    };
+
+    uint8_t NUMBER_SELECT_PINS[NUMBER_OF_SECTORS] {
+        0,3,4,11
+    };
+
+    bool DOTS_STATUS[NUMBER_OF_SECTORS] {
+        false,false,false,false
+    };
+
+    uint8_t NUMBERS[NUMBER_OF_SECTORS] {
+        0,0,0,0
+    };
+
+    void initPins() {
+        for (int i = 0; i < 8; i++) {
+            gpio_init(DATA_PINS[i]);
+            gpio_set_dir(DATA_PINS[i], GPIO_OUT);
+            gpio_put(DATA_PINS[i], 0);
+        }
+        for (int i = 0; i < NUMBER_OF_SECTORS; i++) {
+            gpio_init(NUMBER_SELECT_PINS[i]);
+            gpio_set_dir(NUMBER_SELECT_PINS[i], GPIO_OUT);
+            gpio_put(NUMBER_SELECT_PINS[i], 0);
+        }
+    }
+
+    void put_digits(uint8_t digits) {
+        uint lastNumber = digits % 10;
+        for (int i = 0; i < NUMBER_OF_SECTORS; i++) {
+            NUMBERS[NUMBER_OF_SECTORS - 1 - i] = lastNumber;
+            digits /= 10;
+            lastNumber = digits % ((i+1) * 10);
+        }
+    }
+
+    void displaySector(uint8_t sector) {
+        for (int i = 0; i < NUMBER_OF_SECTORS; i++) gpio_put(NUMBER_SELECT_PINS[i], 0);
+        for (int i = 0; i < 8; i++) gpio_put(DATA_PINS[i], 1);
+
+        gpio_put(NUMBER_SELECT_PINS[sector], 1);
+        
+        if (DOTS_STATUS[sector]) gpio_put(DATA_PINS[7], 0);
+
+        switch (NUMBERS[sector]) {
+            case 0:
+                gpio_put(DATA_PINS[0], 0);
+                gpio_put(DATA_PINS[1], 0);
+                gpio_put(DATA_PINS[2], 0);
+                gpio_put(DATA_PINS[3], 0);
+                gpio_put(DATA_PINS[4], 0);
+                gpio_put(DATA_PINS[5], 0);
+            break;
+            case 1:
+                gpio_put(DATA_PINS[1], 0);
+                gpio_put(DATA_PINS[2], 0);
+            break;
+            case 2:
+                gpio_put(DATA_PINS[0], 0);
+                gpio_put(DATA_PINS[1], 0);
+                gpio_put(DATA_PINS[3], 0);
+                gpio_put(DATA_PINS[4], 0);
+                gpio_put(DATA_PINS[6], 0);
+            break;
+            case 3:
+                gpio_put(DATA_PINS[0], 0);
+                gpio_put(DATA_PINS[1], 0);
+                gpio_put(DATA_PINS[2], 0);
+                gpio_put(DATA_PINS[3], 0);
+                gpio_put(DATA_PINS[6], 0);
+            break;
+            case 4:
+                gpio_put(DATA_PINS[1], 0);
+                gpio_put(DATA_PINS[2], 0);
+                gpio_put(DATA_PINS[6], 0);
+                gpio_put(DATA_PINS[5], 0);
+            break;
+            case 5:
+                gpio_put(DATA_PINS[0], 0);
+                gpio_put(DATA_PINS[5], 0);
+                gpio_put(DATA_PINS[6], 0);
+                gpio_put(DATA_PINS[2], 0);
+                gpio_put(DATA_PINS[3], 0);
+            break;
+            case 6:
+                gpio_put(DATA_PINS[0], 0);
+                gpio_put(DATA_PINS[2], 0);
+                gpio_put(DATA_PINS[3], 0);
+                gpio_put(DATA_PINS[4], 0);
+                gpio_put(DATA_PINS[5], 0);
+                gpio_put(DATA_PINS[6], 0);
+            break;
+            case 7:
+                gpio_put(DATA_PINS[0], 0);
+                gpio_put(DATA_PINS[1], 0);
+                gpio_put(DATA_PINS[2], 0);
+                gpio_put(DATA_PINS[5], 0);
+            break;
+            case 8:
+                gpio_put(DATA_PINS[0], 0);
+                gpio_put(DATA_PINS[1], 0);
+                gpio_put(DATA_PINS[2], 0);
+                gpio_put(DATA_PINS[3], 0);
+                gpio_put(DATA_PINS[4], 0);
+                gpio_put(DATA_PINS[5], 0);
+                gpio_put(DATA_PINS[6], 0);
+            break;
+            case 9:
+                gpio_put(DATA_PINS[0], 0);
+                gpio_put(DATA_PINS[1], 0);
+                gpio_put(DATA_PINS[2], 0);
+                gpio_put(DATA_PINS[3], 0);
+                gpio_put(DATA_PINS[5], 0);
+                gpio_put(DATA_PINS[6], 0);
+            break;
+        }
+            
+    }
+
+    void display_loop() {
+        while (true) {
+            for (int i = 0; i < NUMBER_OF_SECTORS; i++) {
+                displaySector(i);
+                sleep_ms(2);
+            }
+        }
+    }
+
+    void init() {
+        initPins();
+    }
+} 
+
+#endif
